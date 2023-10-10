a9456088baa320a7d820a41167a51e35710cc0d2
Nymphxyz <xyz.hack666@gmail.com>
2023-10-11 10:04:11 +1100

    chore: bump to 269

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
-VELOCITY_JAR_URL=https://api.papermc.io/v2/projects/velocity/versions/3.2.0-SNAPSHOT/builds/265/downloads/velocity-3.2.0-SNAPSHOT-265.jar
+VELOCITY_JAR_URL=https://api.papermc.io/v2/projects/velocity/versions/3.2.0-SNAPSHOT/builds/269/downloads/velocity-3.2.0-SNAPSHOT-269.jar
 
-if curl $VELOCITY_JAR_URL; then
-    # 提取文件名
-    FILENAME=$(basename $URL)
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 DOCKER_USERNAME DOCKER_PASSWORD"
+    exit 1
+fi
 
-    # 从文件名中提取版本号
-    VERSION=$(echo $FILENAME | grep -oP 'velocity-\K[^-]+' | sed 's/.jar//g')
+DOCKER_USERNAME="$1"
+DOCKER_PASSWORD="$2"
+
+if curl -O $VELOCITY_JAR_URL; then
+    FILENAME=$(basename $VELOCITY_JAR_URL)
+
+    VERSION=$(echo "$FILENAME" | sed 's/.jar$//;s/^velocity-//')
     echo "Version: $VERSION"
 
-    sudo docker login -u ${DOCKER_USERNAME} -P ${DOCKER_PASSWORD}
+    sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
     sudo docker build -t velocity:${VERSION} .
-    sudo docker tag velocity:${VERSION} mythoscode/velocity:latest
+    sudo docker tag velocity:${VERSION} mythoscode/velocity:${VERSION}
     sudo docker push mythoscode/velocity:${VERSION}
 
     exit 0
